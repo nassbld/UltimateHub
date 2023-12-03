f10d94612e95f97bbfc403057b9765f27d690b4c
alphakta <alpha94.k@gmail.com>
2023-12-04 00:00:44 +0100

    init architecture app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PlayerComponent } from './components/player/player.component';
+import { CustomPipe } from './shared/custom.pipe';
+import { CustomDirective } from './shared/custom.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PlayerComponent,
+    CustomPipe,
+    CustomDirective
   ],
   imports: [
     BrowserModule,
